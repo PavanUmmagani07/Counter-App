b34c5ca185607e238f77469071590f194840480c
Pkalyan03 <Pavan Ummagani>
2023-03-13 16:29:40 +0000

    Counter APP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import Welcome from './components/Welcome'
-const App = () => {
-  return <Welcome name="Pavan Kalyan" />
-}
+import Counter from './components/Counter'
+
+const App = () => <Counter />
 export default App
